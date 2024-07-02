345afa7d667fea5f928563ba4a8a1edf0052a390
Matthew Riley <mdriley@gmail.com>
2024-07-01 20:20:31 -0700

    Add test cases and code to run them

---
+++ test01.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <optional>
+
+template <typename T>
+constexpr bool kHasUniqueMapOfInstanceType = true;
+
+int main() {
+    constexpr int foo = kHasUniqueMapOfInstanceType<int>? 15: 0;
+    printf("hi\n");
+    return 0;
+}
