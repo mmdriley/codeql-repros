3b7095d9a0ec1244e4441b2166d013da5f87d864
Matthew Riley <mdriley@gmail.com>
2024-07-01 23:17:37 -0700

    Fix test case

--- test03.cc
+++ test03.cc
@@ -1,5 +1,10 @@
+# https://godbolt.org/z/1Ed7bo16j
+
+#include <cstddef>
+#include <type_traits>
+
 template <class _Tp, size_t = 0>
-struct __has_specialization : false_type {};
+struct __has_specialization : std::false_type {};
 
 template <class _Tp>
-struct __has_specialization<_Tp, sizeof(_Tp) * 0> : true_type {};
+struct __has_specialization<_Tp, sizeof(_Tp) * 0> : std::true_type {};
